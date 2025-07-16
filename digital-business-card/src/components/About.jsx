80a23138b75b17ed67e5d257553537f04ac4e646
katisokm10 <130474037+katisokm10@users.noreply.github.com>
2025-07-16 10:09:45 +0200

    Initializing my repo

---
+++ digital-business-card/src/components/About.jsx
@@ -0,0 +1,11 @@
+export default function About () {
+    return (
+        
+        <section>
+            <h2>About</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and I'm always looking for new things to learn</p>
+            <h2>Interests</h2>
+            <p>Pro Football player, Music enthusiast, Foodie, Entrepreneur, Coffee fanatic, Pop culture ninja, Athlete </p>
+        </section>
+    )
+}
---
+++ digital-business-card/src/components/Info.jsx
@@ -0,0 +1,13 @@
+export default function Info() {
+    return(
+        <>
+         <div className="info">
+          <img src='./images/IMG_1054 (1).jpg' /> 
+          <h1 className="info-name">Katiso Mokoena</h1>
+          <p className="job-role">Fullstack Developer</p>
+          <button className="email">Email</button>
+          <button className="linkedln">Linkedln</button>
+         </div>
+        </>
+    )
+}
