28056cb6dccdd29c89486afbfb65beb1976c1db5
katisokm10 <130474037+katisokm10@users.noreply.github.com>
2025-07-16 10:34:44 +0200

    added footer and social media logos

--- digital-business-card/src/components/Footer.jsx
+++ digital-business-card/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer (){
     return (
         <footer>
-            
+            <img src="./images/instagram-167-svgrepo-com.svg" />
+            <img src="./images/facebook-svgrepo-com.svg" />
+            <img src="./images/github-142-svgrepo-com.svg" />
         </footer>
     )
 }
